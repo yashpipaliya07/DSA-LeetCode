6b3e4d5b54e652d3c6e48011fc5caa07136c9511
Yash Pipaliya <yashpipaliya09@gmail.com>
2026-03-02 12:56:53 +0530

    Time: 0 ms (100%), Space: 17.4 MB (99.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,25 +1,76 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        int i=0,j=nums.size()-1;
 
-        while(i<=j){
-            int mid = (i+j)/2;
+    int fo(vector<int>& nums, int target){
+        int l=0;
+        int h=nums.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
             if(nums[mid]==target){
-               int l=mid, r=mid;
-               while(l-1>=0 && nums[l-1]==target) l--;
-               while(r+1<nums.size() && nums[r+1]==target) r++;
-               return {l,r};
+                ans = mid;
+                h=mid-1;
             }
-            else if(nums[mid]<target){
-                i = mid + 1;
+            else if(nums[mid]>target){
+                h=mid-1;
             }
-            else {
-                j = mid - 1;
-            }
+            else l=mid+1;
         }
+        return ans;
+    }
 
-        return {-1,-1};
+    int lo(vector<int>& nums, int target){
+        int l=0;
+        int h=nums.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                l=mid+1;
+            }
+            else if(nums[mid]>target){
+                h=mid-1;
+            }
+            else l=mid+1;
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        // vector<int> v;
+        // int i=0,j=nums.size()-1;
+
+        // while(i<=j){
+        //     int mid = (i+j)/2;
+        //     if(nums[mid]==target){
+        //        int l=mid, r=mid;
+        //        while(l-1>=0 && nums[l-1]==target) l--;
+        //        while(r+1<nums.size() && nums[r+1]==target) r++;
+        //        return {l,r};
+        //     }
+        //     else if(nums[mid]<target){
+        //         i = mid + 1;
+        //     }
+        //     else {
+        //         j = mid - 1;
+        //     }
+        // }
+
+        // return {-1,-1};
+
+        return {fo(nums,target),lo(nums,target)};
+
+
+       
+        //........................STL............................... 
+        
+        // auto first = lower_bound(nums.begin(), nums.end(), target);
+        // auto last = upper_bound(nums.begin(), nums.end(), target);
+
+        // if (first == nums.end() || *first != target)
+        //     return {-1, -1};
+
+        // return {first - nums.begin(), last - nums.begin() - 1};
     }
 };
