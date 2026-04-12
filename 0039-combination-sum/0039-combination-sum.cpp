370097a94995232e04dd9195fae20b50626c8631
Yash Pipaliya <yashpipaliya09@gmail.com>
2026-04-12 17:39:11 +0530

    Time: 19 ms (17.12%), Space: 19.6 MB (17.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> result;
+    set<vector<int>> s;
     void backtrack(int ind,int target,vector<int> &candidates,vector<int> &path,int n){
-        if(ind == n){
-            if(target==0){
-                result.push_back(path);
+        if(target<0 || ind==n) return;
+        if(target==0) {
+            if(s.find(path)==s.end()){
+            result.push_back(path);
+            s.insert(path);
             }
-            return;
+        
         }
-        if(candidates[ind]<=target){
-            path.push_back(candidates[ind]);
-            backtrack(ind,target-candidates[ind],candidates,path,n);
-            path.pop_back();
-        }
-
-        backtrack(ind+1,target,candidates,path,n);
-       
+        path.push_back(candidates[ind]);
+        backtrack(ind+1,target-candidates[ind],candidates,path,n);  //single choice
+        backtrack(ind,target-candidates[ind],candidates,path,n);  // mul choices
+        
+        path.pop_back();
+        backtrack(ind+1,target,candidates,path,n);   //exclusion
+              
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> path;
