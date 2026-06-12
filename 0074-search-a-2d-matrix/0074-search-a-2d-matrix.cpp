c0a93b418edc335090a6ee6af666ef815c82602c
Yash Pipaliya <yashpipaliya09@gmail.com>
2026-06-12 14:10:42 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int t=0;
-        int b=matrix.size()-1;
-        int l=0;
-        int r=matrix[0].size()-1;
-        while(t<=b){
-            if(matrix[(t+b)/2][l]<=target && matrix[(t+b)/2][r]>=target)
-            {
-                while(l<=r){
-                    if(matrix[(t+b)/2][(l+r)/2] == target){
-                        return true;
-                    }
-                    else if(matrix[(t+b)/2][(l+r)/2] > target){
-                        r = (l+r)/2 - 1;
-                    }
-                    else{
-                        l = (l+r)/2 + 1;
-                    }
-                }
-            }
-            else if(matrix[(t+b)/2][l]<target)
-            {
-                t = (t+b)/2 +1;
-            }
-            else
-            {
-                b = (t+b)/2 -1;
-            }
-        }
+        // int t=0;
+        // int b=matrix.size()-1;
+        // int l=0;
+        // int r=matrix[0].size()-1;
+        // while(t<=b){
+        //     if(matrix[(t+b)/2][l]<=target && matrix[(t+b)/2][r]>=target)
+        //     {
+        //         while(l<=r){
+        //             if(matrix[(t+b)/2][(l+r)/2] == target){
+        //                 return true;
+        //             }
+        //             else if(matrix[(t+b)/2][(l+r)/2] > target){
+        //                 r = (l+r)/2 - 1;
+        //             }
+        //             else{
+        //                 l = (l+r)/2 + 1;
+        //             }
+        //         }
+        //     }
+        //     else if(matrix[(t+b)/2][l]<target)
+        //     {
+        //         t = (t+b)/2 +1;
+        //     }
+        //     else
+        //     {
+        //         b = (t+b)/2 -1;
+        //     }
+        // }
 
+        // return false;
+
+        int n =matrix.size();
+        int m = matrix[0].size();
+        int l =0;
+        int h = n*m - 1;
+
+        while(l<=h)
+        {
+            int mid = l + (h-l)/2;
+            if(matrix[mid/m][mid%m]==target) return true;
+            else if(matrix[mid/m][mid%m]>target) h = mid-1;
+            else l = mid+1;
+        }
         return false;
     }
 };
